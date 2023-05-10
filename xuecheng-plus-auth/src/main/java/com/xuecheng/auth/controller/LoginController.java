70892a95e8b07e0cc0a492a5b837161e0804b10c
gushouye <1816886920@qq.com>
2023-05-10 16:59:25 +0800

    :sparkles: 权限控制

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/LoginController.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.xuecheng.ucenter.mapper.XcUserMapper;
 import com.xuecheng.ucenter.model.po.XcUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,16 +36,18 @@ public class LoginController {
         return xcUser;
     }
 
+    // 权限控制
     @RequestMapping("/r/r1")
+    @PreAuthorize("hasAuthority('p1')")
     public String r1() {
         return "访问r1资源";
     }
 
     @RequestMapping("/r/r2")
+    @PreAuthorize("hasAuthority('p2')")
     public String r2() {
         return "访问r2资源";
     }
-
 
 
 }
