2a2a28b881ebe890b1c79b0fb0de26bb17b486bc
gushouye <1816886920@qq.com>
2023-05-18 09:52:24 +0800

    :bug: 查询权限

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserDetailsServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserDetailsServiceImpl.java
@@ -3,9 +3,11 @@ package com.xuecheng.ucenter.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.xuecheng.ucenter.mapper.XcMenuMapper;
 import com.xuecheng.ucenter.mapper.XcUserMapper;
 import com.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.xuecheng.ucenter.model.dto.XcUserExt;
+import com.xuecheng.ucenter.model.po.XcMenu;
 import com.xuecheng.ucenter.model.po.XcUser;
 import com.xuecheng.ucenter.service.AuthService;
 import javafx.application.Application;
@@ -19,6 +21,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author gushouye
  * @description
@@ -31,6 +36,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private XcUserMapper xcUserMapper;
     @Autowired
     private ApplicationContext applicationContext;
+    @Autowired
+    private XcMenuMapper xcMenuMapper;
 
 
     // 传入的请求认证的参数就是AuthParamsDto模型类
@@ -49,6 +56,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         String beanName = authType + "_authService";
         XcUserExt xcuser = applicationContext.getBean(beanName, AuthService.class).execute(authParamsDto);
         // 封装xcUser用户信息为UserDetails
+        // 最终根据userDetails中的信息，生成jwt令牌
         UserDetails userPrincipal = getUserPrincipal(xcuser);
         return userPrincipal;
     }
@@ -59,12 +67,26 @@ public class UserDetailsServiceImpl implements UserDetailsService {
      * @description 查询用户信息
      */
     public UserDetails getUserPrincipal(XcUserExt user) {
+        String password = user.getPassword();
         // 从数据库获取权限
-        String[] permissions = {"test"};
+        String[] authorities = {"test"};
+        // 根据用户id查询用户信息
+        List<XcMenu> xcMenus = xcMenuMapper.selectPermissionByUserId(user.getId());
+        if (xcMenus.size() > 0) {
+//            authorities = new String[xcMenus.size()];
+//            for (int i = 0; i < xcMenus.size(); i++) {
+//                authorities[i] = xcMenus.get(i).getCode();
+//            }
+            List<String> permissions = new ArrayList<>();
+            // 拿到用户拥有的权限标识符
+            xcMenus.forEach(xcMenu -> permissions.add(xcMenu.getCode()));
+            // 转成数组
+            authorities = permissions.toArray(new String[0]);
+        }
         user.setPassword(null);
         // 用户信息转json
         String userJson = JSON.toJSONString(user);
-        UserDetails userDetails = User.withUsername(userJson).password(user.getPassword()).authorities(permissions).build();
+        UserDetails userDetails = User.withUsername(userJson).password(password).authorities(authorities).build();
         return userDetails;
     }
 }
